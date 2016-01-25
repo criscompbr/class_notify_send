f55a2bd494bfcd2d0583537c2c7f7e690a35fda9
Cristiano Thomas <criscompbr@gmail.com>
2016-01-25 16:46:43 -0200

    Correção, adiciona index.php no script simples

--- index.php
+++ index.php
@@ -3,10 +3,14 @@
 require_once './criscomp.notificacao.class.php';
 
 // Exibir apena texto
-//(new criscomp_notify('cristiano'))->enviarNotificacao("Criscomp exemplo", "Exemplo para exibir notificação");
+(new criscomp_notify('cristiano'))->enviarNotificacao("Criscomp exemplo", "Exemplo para exibir notificação");
 
 // Exibir com icone, texto
 //(new criscomp_notify('cristiano'))->enviarNotificacao("Criscomp exemplo", "Exemplo para exibir notificação", "info");
 
 // Exibir com icone, texto e dura 5 segundos para desaparece
-(new criscomp_notify('cristiano'))->enviarNotificacao("Criscomp exemplo", "Exemplo para exibir notificação", "info", 5000);
+//(new criscomp_notify('cristiano'))->enviarNotificacao("Criscomp exemplo", "Exemplo para exibir notificação", "info", 5000);
+
+// Modo script simples
+$notify = new criscomp_notify('cristiano');
+$notify->enviarNotificacao("Criscomp exemplo", "Exemplo para exibir notificação");
